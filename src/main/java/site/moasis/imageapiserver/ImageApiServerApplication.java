57bec9afdb8e7988b409741712d22132a2313fd3
sungwon-097 <csw2757@gmail.com>
2023-01-31 15:39:38 +0900

    #1 SpringBootApplication main()

---
+++ src/main/java/site/moasis/imageapiserver/ImageApiServerApplication.java
@@ -0,0 +1,13 @@
+package site.moasis.imageapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageApiServerApplication.class, args);
+    }
+
+}
