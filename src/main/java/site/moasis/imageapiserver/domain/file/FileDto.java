a669d26b28b94a1d81d575b6a0178a433f08f1c0
sungwon-097 <csw2757@gmail.com>
2023-02-15 11:52:46 +0900

    #9 사용하지 않는 모듈 수정
    데이터베이스 관련 종속성을 삭제

--- src/main/java/site/moasis/imageapiserver/domain/file/FileDto.java
+++ src/main/java/site/moasis/imageapiserver/domain/file/FileDto.java
@@ -1,15 +1,15 @@
-package site.moasis.imageapiserver.domain.file;
-
-import java.io.ByteArrayOutputStream;
-import java.time.LocalDateTime;
-
-public record FileDto (
-){
-    public record fileResponseDto(
-            Long id,
-            String displayName,
-            Integer size,
-            LocalDateTime createAt,
-            ByteArrayOutputStream bas
-    ){}
-}
+//package site.moasis.imageapiserver.domain.file;
+//
+//import java.io.ByteArrayOutputStream;
+//import java.time.LocalDateTime;
+//
+//public record FileDto (
+//){
+//    public record fileResponseDto(
+//            Long id,
+//            String displayName,
+//            Integer size,
+//            LocalDateTime createAt,
+//            ByteArrayOutputStream bas
+//    ){}
+//}
