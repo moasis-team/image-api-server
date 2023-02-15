f26064101bad44bad0f3ccdc5699c6d64150e9c3
sungwon-097 <csw2757@gmail.com>
2023-02-15 11:53:14 +0900

    #9 이미지 컨버터 설정 추가

---
+++ src/main/java/site/moasis/imageapiserver/configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package site.moasis.imageapiserver.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new BufferedImageHttpMessageConverter());
+    }
+}
