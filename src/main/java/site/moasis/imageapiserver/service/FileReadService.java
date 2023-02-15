16ddb725e10012adc051f9eef7be29d955810d7f
sungwon-097 <csw2757@gmail.com>
2023-02-15 11:54:23 +0900

    #9 이미지 서버 기능 변경
    - DB는 moasis-be에서 관리
    - 파일 조회는 디렉토리에 저장된 내용 반환
    - 리사이즈시 비율을 변경해 반환. 후에 뷰의 크기를 고려해 변경해야함

--- src/main/java/site/moasis/imageapiserver/service/FileReadService.java
+++ src/main/java/site/moasis/imageapiserver/service/FileReadService.java
@@ -3,12 +3,7 @@ package site.moasis.imageapiserver.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import site.moasis.imageapiserver.common.exception.BusinessException;
-import site.moasis.imageapiserver.common.exception.ErrorCode;
 import site.moasis.imageapiserver.configuration.FilePath;
-import site.moasis.imageapiserver.domain.file.FileMetaData;
-import site.moasis.imageapiserver.infrastructure.repository.UploadRepository;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
@@ -16,39 +11,49 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class FileReadService {
 
-    private final UploadRepository uploadRepository;
     private final FilePath filePath;
 
-    public List<FileMetaData> allFiles() {
-        return uploadRepository.findAll();
-    }
-
-    public ByteArrayOutputStream getResizedImage(Integer width, Integer height, String file) throws IOException {
-
-        uploadRepository.findByDisplayName(file).orElseThrow(() ->
-                new BusinessException(ErrorCode.NOT_FOUND, "images.notExist", List.of(file)));
+    public ByteArrayOutputStream getResizedImage(Integer percent, String file) throws IOException {
 
         BufferedImage originalImage = ImageIO.read(new File(filePath.getPath() + file));
 
-        if (width.equals(0)) width = originalImage.getWidth();
-        if (height.equals(0)) height = originalImage.getHeight();
+        int width = (int) (originalImage.getWidth() * percent * 0.01f);
+        int height = (int) (originalImage.getWidth() * percent * 0.01f);
 
         BufferedImage resizedImage = new BufferedImage(width, height, originalImage.getType());
-        Graphics2D g = resizedImage.createGraphics();
-        g.drawImage(originalImage, 0, 0, width, height, null);
-        g.dispose();
+        resize(originalImage, width, height, resizedImage);
 
+        return getByteArrayOutputStream(resizedImage);
+    }
+
+    public List<String> getDirFiles(File f) {
+
+        List<String> fileList = new ArrayList<>();
+        File[] files = f.listFiles();
+        for (File file : files)
+            fileList.add(file.getName());
+
+        return fileList;
+    }
+
+    private ByteArrayOutputStream getByteArrayOutputStream(BufferedImage resizedImage) throws IOException {
         ByteArrayOutputStream bas = new ByteArrayOutputStream();
         ImageIO.write(resizedImage, "png", bas);
         bas.flush();
         return bas;
+    }
+
+    private void resize(BufferedImage originalImage, int width, int height, BufferedImage resizedImage) {
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(originalImage, 0, 0, width, height, null);
+        g.dispose();
     }
 }
--- src/main/java/site/moasis/imageapiserver/service/FileWriteService.java
+++ src/main/java/site/moasis/imageapiserver/service/FileWriteService.java
@@ -5,14 +5,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 import site.moasis.imageapiserver.common.exception.ErrorCode;
 import site.moasis.imageapiserver.common.exception.FileIOException;
 import site.moasis.imageapiserver.configuration.FilePath;
-import site.moasis.imageapiserver.domain.file.FileMetaData;
-import site.moasis.imageapiserver.infrastructure.repository.UploadRepository;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,17 +17,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.time.LocalDateTime;
 import java.util.Objects;
-import java.util.UUID;
 
 @Slf4j
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class FileWriteService {
 
-    private final UploadRepository uploadRepository;
     private final FilePath filePath;
     private Path fileDir;
 
@@ -46,42 +39,44 @@ public class FileWriteService {
 
     public String uploadFile(MultipartFile file) throws FileIOException {
         String allowExtends = "png, jpg, jpeg, gif";
+        checkExtends(file, allowExtends);
 
+        return storeFile(file);
+    }
+
+    public HttpStatus deleteImage(String name) {
+        File target = new File(filePath.getPath() + name);
+
+        return getStatusAfterDelete(target);
+    }
+
+    private void checkExtends(MultipartFile file, String allowExtends) throws FileIOException {
         if (!allowExtends.contains(Objects.requireNonNull(file.getContentType()).split("/")[1]))
             throw new FileIOException(ErrorCode.INVALID_PARAMETER);
+    }
 
+    private String storeFile(MultipartFile file) throws FileIOException {
         String uploadFileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
-        String storedName = UUID.randomUUID() + uploadFileName;
 
-        Path targetLocation = fileDir.resolve(storedName);
+        Path targetLocation = fileDir.resolve(uploadFileName);
         try {
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             throw new FileIOException(ErrorCode.FILE_IO_ERROR);
         }
 
-        FileMetaData uploadFile = FileMetaData.builder()
-                .displayName(storedName)
-                .size((int) file.getSize())
-                .uploadDateTime(LocalDateTime.now())
-                .build();
         try {
-            file.transferTo(new File(filePath.getPath() + storedName));
-        }catch (IOException e){
+            file.transferTo(new File(filePath.getPath() + uploadFileName));
+        } catch (IOException e) {
             throw new FileIOException(ErrorCode.INTERNAL_SERVER_ERROR);
-        }finally {
-            uploadRepository.save(uploadFile);
         }
-        return uploadFile.getDisplayName();
+        return uploadFileName;
     }
 
-    public HttpStatus deleteImage(String file) {
-        uploadRepository.deleteByDisplayName(file);
-        File target = new File(filePath.getPath()+ file);
-
+    private HttpStatus getStatusAfterDelete(File target) {
         HttpStatus httpStatus;
-        if( target.exists() )
-            if(target.delete())
+        if (target.exists())
+            if (target.delete())
                 httpStatus = HttpStatus.OK;
             else
                 httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
